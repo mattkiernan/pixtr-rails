0e68f2ef643c99c2d66632a18c11abbb4bff985a
mattkiernan <matt.kiernan@gmail.com>
2014-09-29 15:24:26 -0400

    Users can edit gallery names

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -14,7 +14,6 @@ class GalleriesController < ApplicationController
      name: params[:gallery][:name],
      description: params[:gallery][:description]
    )
-
    redirect_to "/"
  end
 
@@ -28,4 +27,16 @@ class GalleriesController < ApplicationController
    @gallery = Gallery.find(params[:id])
    render :edit
  end
+
+ def update
+  gallery= Gallery.find(params[:id])
+  gallery.update(
+    name: params[:gallery][:name],
+    description: params[:gallery][:description]
+  )
+    redirect_to "/"
+ end 
+  
+
+
 end
