6d46ca7a1f084c8c750a277edf3cb25ea6590b34
mattkiernan <matt.kiernan@gmail.com>
2014-10-01 12:51:13 -0400

    Add support for editting photos; Add support for deleting photos

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -13,18 +13,30 @@ class ImagesController < ApplicationController
   def create
     @gallery = Gallery.find(params[:gallery_id])
     @image = @gallery.images.create(image_params) 
-    redirect_to "/"
+    redirect_to gallery_path(@gallery) 
   end
 
   def image_params
     params.require(:image).permit(:name, :url)
   end
 
-  def destroy
+  def edit
     @gallery = Gallery.find(params[:gallery_id])
-    @image = Image.find(params[:id])
-    @image.destroy 
-    redirect_to "/"
+    @image = @gallery.images.find(params[:id])
+  end
+
+  def destroy
+    gallery = Gallery.find(params[:gallery_id])
+    image = Image.find(params[:id])
+    image.destroy 
+    redirect_to gallery_path(gallery) 
+  end
+
+  def update
+    gallery = Gallery.find(params[:gallery_id])
+    image = gallery.images.find(params[:id])
+    image.update(image_params)
+    redirect_to gallery_path(gallery) 
   end
 
 end
