2b935c96d100a3541e0afdd1ec531b7bb3937525
mattkiernan <matt.kiernan@gmail.com>
2014-10-02 16:23:35 -0400

    Require current_user to view galleries & images

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,17 +1,17 @@
 class ImagesController < ApplicationController
 
   def show
-    @gallery = Gallery.find(params[:gallery_id])
+    @gallery =  load_gallery_from_url 
     @image = @gallery.images.find(params[:id])
   end
 
   def new
-    @gallery = Gallery.find(params[:gallery_id])
+    @gallery =  load_gallery_from_url 
     @image = @gallery.images.new
   end
 
   def create
-    @gallery = Gallery.find(params[:gallery_id])
+    @gallery =  load_gallery_from_url 
     @image = @gallery.images.new(image_params)
     if @image.save
       redirect_to gallery_image_path(@gallery, @image)
@@ -25,25 +25,29 @@ class ImagesController < ApplicationController
   end
 
   def edit
-    @gallery = Gallery.find(params[:gallery_id])
+    @gallery =  load_gallery_from_url 
     @image = @gallery.images.find(params[:id])
   end
 
   def destroy
-    gallery = Gallery.find(params[:gallery_id])
+    @gallery =  load_gallery_from_url 
     image = Image.find(params[:id])
     image.destroy 
     redirect_to gallery_path(gallery) 
   end
 
   def update
-    @gallery = Gallery.find(params[:gallery_id])
+    @gallery =  load_gallery_from_url 
     @image = @gallery.images.find(params[:id])
     if @image.update(image_params)
       redirect_to gallery_image_path(@gallery, @image) 
     else
       render :edit
     end
+  end
+
+  def load_gallery_from_url
+    current_user.galleries.find(params[:gallery_id])
   end
 
 end
